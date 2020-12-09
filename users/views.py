67546015b5ce3ff3868ec607373bfd260dd5ad67
jimbobding <jimmygrahamdavid@gmail.com>
2020-12-09 16:40:57 +0000

    completed crud functionality for posts

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{"Your account has been created!You are now able to log in"}')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -27,8 +27,9 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'{"Your account has been updated!"}')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
@@ -37,4 +38,5 @@ def profile(request):
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
