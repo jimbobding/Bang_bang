dea4d25fb21b34801f1643cdcee8a6d025c6dea9
jimbobding <jimmygrahamdavid@gmail.com>
2020-12-22 22:51:21 +0000

    connected database

--- bang_bang/settings.py
+++ bang_bang/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 
 if os.path.exists('env.py'):
     import env
-    
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,14 +86,17 @@ WSGI_APPLICATION = 'bang_bang.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
