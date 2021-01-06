f553d02897f20ee811d4ee9aba0c938c09374f23
jimbobding <jimmygrahamdavid@gmail.com>
2021-01-06 22:49:03 +0000

    changed spelling mistake in from email

--- bang_bang/settings.py
+++ bang_bang/settings.py
@@ -177,7 +177,7 @@ LOGIN_URL = 'home'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'jimgrahamtest@gexample.com'
+    DEFAULT_FROM_EMAIL = 'jimgrahamtest@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
