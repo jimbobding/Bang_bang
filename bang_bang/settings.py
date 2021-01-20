f3f7cf199b6fc3c177ed10189c6880f897058bfc
jimbobding <jimmygrahamdavid@gmail.com>
2021-01-20 18:14:46 +0000

    deleted users app

--- bang_bang/settings.py
+++ bang_bang/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'profiles.apps.UsersConfig',
+    'profiles'
 
 ]
 
--- bang_bang/urls.py
+++ bang_bang/urls.py
@@ -17,7 +17,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
+
 from home import views as home_views
 
 
